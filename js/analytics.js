f4bb7682e0130946fe5310b76e2b98be5ccd480f
zichun <zhuzichunhogan@gmail.com>
2024-09-06 10:58:33 +1000

    add my domain and web statistic

---
+++ js/analytics.js
@@ -0,0 +1,13 @@
+// Google Analytics
+(function() {
+    var script = document.createElement('script');
+    script.async = true;
+    script.src = "https://www.googletagmanager.com/gtag/js?id=G-R8R2BBST9R";
+    document.head.appendChild(script);
+    
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-R8R2BBST9R');
+})();
